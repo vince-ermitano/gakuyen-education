d141c3ce8ec704f14fad53a053e3cc2eb96c655b
Prince Vince <vincexermitano@gmail.com>
2023-07-31 01:14:28 -0700

    Update Hamburger dropdown to include 3 main categories

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -14,12 +14,43 @@ const Hamburger = () => {
                         <div className="bottom"></div>
                     </label>
 
-                    <nav className="menu1">
-                        {/* <a className="link1" href="/">Home</a> */}
+                    {/* <nav className="menu1">
                         <Link className="link1" to="/">Home</Link>
                         <a className="link1" href="/">Presets</a>
                         <a className="link1" href="/">Masterclass</a>
                         <a className="link1" href="/">Contact</a>
+                    </nav> */}
+                    <nav className="menu1">
+                        <div className="shop-by-category">
+                            <h3>Shop By</h3>
+                            <hr />
+                            <div className="shop-by-category-links">
+                                <Link className="link1" to="/store">Presets</Link>
+                                <Link className="link1" to="/store">Masterclass</Link>
+                                <Link className="link1" to="/">Editing</Link>
+                                <Link className="link1" to="/">Workflow</Link>
+                                <Link className="link1" to="/">Connection</Link>
+                                <Link className="link1" to="/">Gaku's Favorites</Link>
+                            </div>
+                        </div>
+                        <div className="explore-category">
+                            <h3>Explore</h3>
+                            <hr />
+                            <div className="explore-category-links">
+                                <Link className="link1" to="/">Home</Link>
+                                <Link className="link1" to="/">Free Resources</Link>
+                                <Link className="link1" to="/">About Gaku</Link>
+                            </div>
+                        </div>
+                        <div className="connect-category">
+                            <h3>Connect</h3>
+                            <hr />
+                            <div className="connect-category-links">
+                                <Link className="link1" to="/">Get in Touch</Link>
+                                <Link className="link1" to="/">Frequent Questions</Link>
+                                <Link className="link1" to="/">My Account</Link>
+                            </div>
+                        </div>
                     </nav>
                 </nav>
             </section>
