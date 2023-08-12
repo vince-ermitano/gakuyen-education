a310ea6a2768ed3461a4ac125ef449f34cdafacf
Prince Vince <vincexermitano@gmail.com>
2023-08-12 01:28:31 -0700

    Add ability to navigate to item description page for items in the Best Seller section by adding props to <BestSellersItem />

--- src/components/BestSellers/BestSellers.js
+++ src/components/BestSellers/BestSellers.js
@@ -7,8 +7,8 @@ const BestSellers = () => {
         <div className="best-sellers page-section">
             <h2>Best Sellers</h2>
             <div className="item-container">
-                <BestSellersItem itemName="ultimate transition pack" imagePath="/ultimate_transition_pack_1_400x400.jpg" />
-                <BestSellersItem itemName="ultimate preset pack" imagePath="/ultimate_preset_pack_1_400x400.jpg"/>
+                <BestSellersItem itemType="preset" itemName="ultimate transition pack" imagePath="/ultimate_transition_pack_1_400x400.jpg" />
+                <BestSellersItem itemType="preset" itemName="ultimate preset pack" imagePath="/ultimate_preset_pack_1_400x400.jpg"/>
                 <BestSellersItem />
                 <BestSellersItem />
             </div>
--- src/components/BestSellers/BestSellersItem.js
+++ src/components/BestSellers/BestSellersItem.js
@@ -1,13 +1,24 @@
 import React from "react";
 import './BestSellersItem.css'
+import { useNavigate} from "react-router-dom";
 
 const BestSellersItem = (props) => {
 
     const itemName = props.itemName;
     const itemImagePath = props.imagePath;
+    const itemType = props.itemType;
+
+    const navigate = useNavigate();
+
+    const redirectToPresetDesc = () => {
+        if (itemType === "preset") {
+            console.log("redirecting to preset desc");
+            navigate("/preset-desc");
+        }
+    }
 
     return (
-        <div className="item">
+        <div className="item" onClick={() => redirectToPresetDesc()}>
             <div className="item-image">
                 <img src={itemImagePath ? itemImagePath : "https://archive.org/download/placeholder-image/placeholder-image.jpg" } alt="item" />
             </div>
