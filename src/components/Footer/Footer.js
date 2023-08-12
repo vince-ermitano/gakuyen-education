b7d455190e72db8d8f6862e1881d6ad89de6cf7b
Prince Vince <vincexermitano@gmail.com>
2023-08-12 00:41:52 -0700

    Update footer logo to The Odyssey logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './Footer.css';
-import { FaCode } from 'react-icons/fa';
+// import { FaCode } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <div className="footer page-section">
-            <FaCode style={{ fontSize: 32 }}/>
+            {/* <FaCode style={{ fontSize: 32 }}/> */}
+            <div className="footer-logo">
+                <img src="/theodyssey_s.png" alt="Gakuyen Education Logo" />
+            </div>
             <p>Lorem ipsum dolor sit amet</p>
             <p>Consectetur adipiscing elit, sed do eiusmod tempor </p>
             <p>Incididunt ut labore et dolore magna aliqua</p>
