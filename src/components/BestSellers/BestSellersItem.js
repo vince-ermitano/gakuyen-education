a63d0a149604e17032f7aa07e09a7fc9c1e2edd3
Prince Vince <vincexermitano@gmail.com>
2023-08-12 01:18:33 -0700

    Add some items to the BestSellers section on homepage

--- src/components/BestSellers/BestSellersItem.js
+++ src/components/BestSellers/BestSellersItem.js
@@ -1,14 +1,18 @@
 import React from "react";
 import './BestSellersItem.css'
 
-const BestSellersItem = () => {
+const BestSellersItem = (props) => {
+
+    const itemName = props.itemName;
+    const itemImagePath = props.imagePath;
+
     return (
         <div className="item">
             <div className="item-image">
-                <img src="https://archive.org/download/placeholder-image/placeholder-image.jpg" alt="item" />
+                <img src={itemImagePath ? itemImagePath : "https://archive.org/download/placeholder-image/placeholder-image.jpg" } alt="item" />
             </div>
             <div className="item-info">
-                <span className="item-name">The Odyssey - Creative<br></br>Masterclass</span>
+                <span className="item-name">{itemName ? itemName : "The Odyssey - Creative\nMasterclass"}</span>
             </div>
         </div>
     )
