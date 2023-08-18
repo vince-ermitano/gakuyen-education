abeb36ddc5ddeaf146a2ad8000784a3e5f016662
Prince Vince <vincexermitano@gmail.com>
2023-08-17 19:21:19 -0700

    Add right side contents of ModuleView along with padding and styling

--- src/components/Dashboard/ModuleView/ModuleView.js
+++ src/components/Dashboard/ModuleView/ModuleView.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactPlayer from "react-player";
 import "./ModuleView.css";
 import ModulePreview from "./ModulePreview";
 
@@ -35,7 +36,40 @@ const ModuleView = () => {
                     <ModulePreview />
                 </div>
             </div>
-            <p>Right Side</p>
+            <div className="module-content">
+                <div className="video-player-wrapper">
+                    <ReactPlayer
+                        className="react-player"
+                        url="https://www.youtube.com/watch?v=fyNns5amxRk"
+                        width="100%"
+                        height="100%"
+                        controls
+                        playing
+                    />
+                </div>
+                <h1>Module Title</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+                <h2>Attached Files</h2>
+                <p>text</p>
+                <p>text</p>
+                <p>text</p>
+                <p>text</p>
+                <p>text</p>
+                <p>text</p>
+                <p>text</p>
+                <p>text</p>
+                <p>text</p>
+                <p>text</p>
+            </div>
         </div>
     );
 };
