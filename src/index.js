50ec32d96f7d11d9c29e6d301802a8a3a0fa1b0d
Prince Vince <vincexermitano@gmail.com>
2023-08-29 19:46:59 -0700

    Initialize 'cart' in localStorage when users visit site for the first time

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store/store';
 
+// Set initial cart in localStorage
+if (!localStorage.getItem('cart')) {
+    localStorage.setItem('cart', JSON.stringify([]));
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
