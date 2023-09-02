805d2dc0f2fb5ee8d12cd49fcb2091d34cb09e61
Prince Vince <vincexermitano@gmail.com>
2023-09-01 18:22:06 -0700

    Add checkout functionality using Stripe: 	- create an Express server (server.js) 	- make 'Proceed to Checkout' button in Cart to redirect to Stripe payment page 		* NOTE: this functionality will not work on the live server, yet, as Netlify doesn't support backend functionality

---
+++ server.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "client/build")));
+
+//     // Handle any requests that don't match API routes by serving the React app
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//     });
+// }
+
+const storeItems = new Map([
+    [1, { priceInCents: 10000, name: "Learn React Today" }],
+    [2, { priceInCents: 20000, name: "Learn CSS Today" }],
+  ]);
+
+app.post("/create-checkout-session", async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: "payment",
+            line_items: req.body.items.map((item) => {
+                const storeItem = storeItems.get(item.id);
+                return {
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: storeItem.name,
+                        },
+                        unit_amount: storeItem.priceInCents,
+                    },
+                    quantity: item.quantity,
+                };
+            }),
+            success_url: `${process.env.SERVER_URL}`,
+            cancel_url: `${process.env.SERVER_URL}`,
+        });
+        res.json({ url: session.url });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
