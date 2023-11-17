4d2b310abe6804949dbad1a55c1df0121fcfb9e3
Prince Vince <vincexermitano@gmail.com>
2023-11-17 10:08:39 -0800

    Setup password page for whole website

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import './App.css';
-import { useEffect } from 'react';
-import Header from './components/Header/Header_logged_out';
-import Shop from './components/Shop/Shop';
-import store from './store/store';
-import { Provider, useDispatch } from 'react-redux';
-import Homepage from './components/Homepage/Homepage';
-import HamburgerMenu from './components/Hamburger/HamburgerMenu';
-import { Routes, Route, useLocation } from "react-router-dom"; 
-import Footer from './components/Footer/Footer';
-import PresetDescPage from './components/Presets/PresetDescPage';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./components/Header/Header_logged_out";
+import Shop from "./components/Shop/Shop";
+import store from "./store/store";
+import { Provider, useDispatch } from "react-redux";
+import Password from "./components/Password/Password";
+import Homepage from "./components/Homepage/Homepage";
+import HamburgerMenu from "./components/Hamburger/HamburgerMenu";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import PresetDescPage from "./components/Presets/PresetDescPage";
 // import Hamburger from './components/Hamburger/Hamburger_v2';
-import MasterclassDesc from './components/Masterclass/MasterclassDesc';
-import LoginSidebar from './components/Sidebar/LoginSidebar';
-import CreateAccountSidebar from './components/Sidebar/CreateAccountSidebar';
-import CartSidebar from './components/Sidebar/CartSidebar';
+import MasterclassDesc from "./components/Masterclass/MasterclassDesc";
+import LoginSidebar from "./components/Sidebar/LoginSidebar";
+import CreateAccountSidebar from "./components/Sidebar/CreateAccountSidebar";
+import CartSidebar from "./components/Sidebar/CartSidebar";
 // import HeaderHerov2 from './components/HeaderHerov2/HeaderHerov2';
-import Dashboard from './components/Dashboard/Dashboard';
-import ModuleView from './components/Dashboard/ModuleView/ModuleView';
-import VideoView from './components/Dashboard/ModuleView/VideoView';
-import Settings from './components/Dashboard/Settings/Settings';
-import PresetLutView from './components/Dashboard/PresetLutView/PresetLutView';
-import Contact from './components/Contact/Contact';
-import { setLoggedInStatus } from './features/LoggedInStatusSlice';
-import { auth } from './config/firebaseConfig';
-import { onAuthStateChanged } from 'firebase/auth';
-import { db } from "./config/firebaseConfig"
+import Dashboard from "./components/Dashboard/Dashboard";
+import ModuleView from "./components/Dashboard/ModuleView/ModuleView";
+import VideoView from "./components/Dashboard/ModuleView/VideoView";
+import Settings from "./components/Dashboard/Settings/Settings";
+import PresetLutView from "./components/Dashboard/PresetLutView/PresetLutView";
+import Contact from "./components/Contact/Contact";
+import { setLoggedInStatus } from "./features/LoggedInStatusSlice";
+import { auth } from "./config/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { db } from "./config/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
-import { setProducts, setLoading, setInitialTotalPrice} from './features/ShopSlice';
-import { setUserInfo } from './features/UserSlice';
+import {
+    setProducts,
+    setLoading,
+    setInitialTotalPrice,
+} from "./features/ShopSlice";
+import { setUserInfo } from "./features/UserSlice";
 // import { ToastContainer, toast } from "react-toastify";
-import DashboardHome from './components/Dashboard/DashboardHome/DashboardHome';
+import DashboardHome from "./components/Dashboard/DashboardHome/DashboardHome";
 import { Toaster, toast } from "sonner";
 
-
-function App () {
-
+function App() {
     /**
-     * TODO: 
+     * TODO:
      * - update '/store' path to '/store/:filter' path
      */
     const location = useLocation();
@@ -48,23 +51,33 @@ function App () {
     const shouldHideComponents = currentPath.includes(dashboardPath);
 
     const dispatch = useDispatch();
+    const [authenticated, setAuthenticated] = useState(false);
 
-    const gradientCanvas = document.getElementById("gradient-canvas");
-    const header = document.querySelector(".hero");
-    
-    // if the current path is not the homepage, hide the gradient canvas
-    if (currentPath !== "/") {
-        gradientCanvas.style.display = "none";
-        header.style.display = "none";
-    } else {
-        gradientCanvas.style.display = "block";
-        header.style.display = "block";
-    }
     // useEffect(() => {
-    // }, [currentPath]);
+        // }, [currentPath]);
+        
+    useEffect(() => {
+        // TODO: might need to move this into a useEffect hook
+        const gradientCanvas = document.getElementById("gradient-canvas");
+        const header = document.querySelector(".hero");
+
+        // if the current path is not the homepage, hide the gradient canvas
+        if (currentPath !== "/" || !authenticated) {
+            gradientCanvas.style.display = "none";
+            header.style.display = "none";
+        } else {
+            gradientCanvas.style.display = "block";
+            header.style.display = "block";
+        }
+    }, [authenticated, currentPath]);
 
     useEffect(() => {
-        const userDirectoryLinks = document.querySelectorAll(".user-directory span");
+
+        if (!authenticated) return;
+
+        const userDirectoryLinks = document.querySelectorAll(
+            ".user-directory span"
+        );
         const logo = document.querySelector(".header .logo img");
         const cartSvg = document.querySelector(".header .user-directory svg");
 
@@ -73,30 +86,32 @@ function App () {
         }
 
         if (currentPath !== "/") {
-            userDirectoryLinks.forEach(link => {
+            userDirectoryLinks.forEach((link) => {
                 link.style.color = "black";
             });
             logo.src = "/theodyssey_s.png";
             cartSvg.style.color = "black";
         } else {
-            userDirectoryLinks.forEach(link => {
+            userDirectoryLinks.forEach((link) => {
                 link.style.color = "white";
             });
             logo.src = "/theodysseywhite_s.png";
             cartSvg.style.color = "white";
         }
-    }, [currentPath])
+    }, [authenticated, currentPath]);
 
     // useEffect(() => {
 
     // })
 
     useEffect(() => {
+        if (!authenticated) return;
+
         const dashboardText = document.querySelector(".user-directory a");
 
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
-                if (currentPath !== '/') {
+                if (currentPath !== "/") {
                     // dashboardText?.style.color = "black";
                     if (dashboardText) {
                         dashboardText.style.color = "black";
@@ -111,12 +126,13 @@ function App () {
 
         return () => {
             unsubscribe();
-        }
-
-    }, [currentPath])
+        };
+    }, [authenticated, currentPath]);
 
     useEffect(() => {
         // Listen for authentication state changes
+        if (!authenticated) return;
+
 
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
@@ -129,34 +145,33 @@ function App () {
 
         const getUserInfo = async () => {
             auth.currentUser
-            .getIdToken(true)
-            .then((idToken) => {
-                fetch(
-                    `${process.env.REACT_APP_SERVER_URL}/user-info`,
-                    {
+                .getIdToken(true)
+                .then((idToken) => {
+                    fetch(`${process.env.REACT_APP_SERVER_URL}/user-info`, {
                         method: "GET",
                         headers: {
                             Authorization: `Bearer ${idToken}`,
                             "Content-Type": "application/json",
                         },
-                    }
-                )
-                    .then((res) => {
-                        if (res.ok) return res.json();
-                        return res.text().then((error) => Promise.reject(error));
                     })
-                    .then((res) => {
-                        dispatch(setUserInfo(res));
-                    })
-                    .catch((e) => {
-                        console.error(e);
-                        toast.error(e);
-                    });
-            })
-            .catch((e) => {
-                console.error(e.error);
-            });
-        }
+                        .then((res) => {
+                            if (res.ok) return res.json();
+                            return res
+                                .text()
+                                .then((error) => Promise.reject(error));
+                        })
+                        .then((res) => {
+                            dispatch(setUserInfo(res));
+                        })
+                        .catch((e) => {
+                            console.error(e);
+                            toast.error(e);
+                        });
+                })
+                .catch((e) => {
+                    console.error(e.error);
+                });
+        };
 
         // Get products from Firestore
         const getProducts = async () => {
@@ -174,8 +189,7 @@ function App () {
                 dispatch(setInitialTotalPrice());
                 dispatch(setLoading(false));
 
-                console.log('received products from firestore');
-
+                console.log("received products from firestore");
             } catch (error) {
                 console.log(error);
                 // toast.error("Error getting products", {
@@ -197,7 +211,11 @@ function App () {
             // Unsubscribe when the component unmounts
             unsubscribe();
         };
-    }, [dispatch]);
+    }, [authenticated, dispatch]);
+
+    if (!authenticated) {
+        return <Password setAuthenticated={setAuthenticated} />;
+    }
 
     return (
         <Provider store={store}>
@@ -240,17 +258,23 @@ function App () {
                 ></Route>
                 <Route path="/dashboard" element={<Dashboard />}>
                     <Route path="main" element={<DashboardHome />}></Route>
-                    <Route path="modules/:moduleId/videos" element={<VideoView />}></Route>
+                    <Route
+                        path="modules/:moduleId/videos"
+                        element={<VideoView />}
+                    ></Route>
                     <Route path="modules" element={<ModuleView />}></Route>
                     <Route path="presets" element={<PresetLutView />}></Route>
                     <Route path="luts" element={<PresetLutView />}></Route>
-                    <Route path="transitions" element={<PresetLutView />}></Route>
+                    <Route
+                        path="transitions"
+                        element={<PresetLutView />}
+                    ></Route>
                     <Route path="settings" element={<Settings />}></Route>
                 </Route>
                 <Route path="/contact" element={<Contact />}></Route>
             </Routes>
 
-            <Toaster position='top-center' richColors />
+            <Toaster position="top-center" richColors />
             {/* <ToastContainer theme="dark" style={{ width: "500px" }} /> */}
             {!shouldHideComponents && <Footer />}
         </Provider>
---
+++ src/components/Password/Password.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Password = (props) => {
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (password === process.env.REACT_APP_WEBSITE_PW) {
+            props.setAuthenticated(true);
+        } else return;
+    };
+    return (
+        <form id="password-page" onSubmit={handleSubmit}>
+            <img src="/theodyssey_s.png" alt="The Odyssey" />
+            <input
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default Password;
