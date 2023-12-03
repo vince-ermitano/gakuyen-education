56ae630b8cca307d8043abf440262c4b80bb8f7b
Prince Vince <vincexermitano@gmail.com>
2023-12-02 22:06:29 -0800

    Add newsletter signup to Password page

--- src/components/Password/Password.js
+++ src/components/Password/Password.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { Toaster, toast } from "sonner";
 
 const Password = (props) => {
     const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -10,18 +12,64 @@ const Password = (props) => {
             props.setAuthenticated(true);
         } else return;
     };
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+
+        fetch(`${process.env.REACT_APP_SERVER_URL}/newsletter-signup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email,
+            }),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        "Error in fetch request: (newsletter signup)"
+                    );
+                }
+                return response.text();
+            })
+            .then((data) => {
+                toast.success("Email added to newsletter successfully!");
+                setEmail("");
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                toast.error("Failed to add email to newsletter. Please try again.");
+            });
+    }
+
     return (
-        <form id="password-page" onSubmit={handleSubmit}>
-            <img src="/theodyssey.png" alt="The Odyssey" />
-            {/* <Logo /> */}
-            <input
-                type="password"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Submit</button>
-        </form>
+        <section id="password-and-signup">
+            <form id="password-page" onSubmit={handleSubmit}>
+                <img src="/theodyssey.png" alt="The Odyssey" />
+                {/* <Logo /> */}
+                <input
+                    type="password"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            <form id="signup-form" onSubmit={handleSignup}>
+                <label htmlFor="email">Don't miss out!</label>
+                <input
+                    id="email"
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <button type="submit">Submit</button>
+            </form>
+            <Toaster position="top-center" richColors />
+        </section>
     );
 };
 
