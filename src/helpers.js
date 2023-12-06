863223a990831ddb424b1d63967dcbd49fb26dc4
Prince Vince <vincexermitano@gmail.com>
2023-12-05 20:30:53 -0800

    Fix background fill for cart on mobile devices when toggling the hamburger menu

--- src/helpers.js
+++ src/helpers.js
@@ -183,6 +183,8 @@ export const toggleHamburger = (path) => {
         }
         enableScroll();
     }
+
+    updateBackgroundColorBasedOnWindowSize();
 };
 
 export const changeHeaderTextAndLogoToColor = (color) => {
